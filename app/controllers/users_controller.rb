fb5c786c1235b79dc1a3b27f0c6d20b0d4f5bb54
philmphoenix <jpholzmann@gmail.com>
2016-01-11 10:47:11 -0700

    Adds users controller to handle the creation of users.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(session[:current_user_id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:current_user_id] = user.id
+      redirect_to links_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    redirect_to login_path
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
